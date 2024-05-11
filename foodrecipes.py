74ce18e46e0c9be3392b18fbf5b9374e4d6a4f07
fm-lopez <166548237+fm-lopez@users.noreply.github.com>
2024-05-10 17:37:58 -0700

    Add files via upload

---
+++ foodrecipes.py
@@ -0,0 +1,300 @@
+foodrecipes = {
+    "vegetables": {
+        "Veggie-Packed Okonomiyaki": {
+            "image_id": "image_id",
+            "ingredients": [
+                "¼ cup mayonnaise",
+                "1 teaspoon Sriracha",
+                "¼ teaspoon toasted sesame oil",
+                "2 cups shredded green cabbage",
+                "2 cups shredded zucchini",
+                "1 cup shredded carrot",  
+                "4 scallions, sliced, whites and greens separated, divided",
+                "¼ cup brown or white rice flour",
+                "½ teaspoon kosher salt",
+                "2 large eggs",
+                "2 tablespoons canola oil",
+                "2 teaspoons toasted sesame seeds"
+            ],  
+            "directions": [
+                "Step 1: Whisk mayonnaise, Sriracha and sesame oil in a small bowl. Set aside.\n", 
+                "Step 2: Combine cabbage, zucchini, carrot and scallion whites in a large bowl. Sprinkle with rice flour and salt and toss to coat. Beat eggs in a small bowl, then pour over the vegetables and stir until evenly combined.\n",
+                "Step 3: Heat canola oil in a large nonstick skillet over medium-high heat until shimmering. Add the vegetable mixture, pressing gently on the top and sides with a spatula to shape it into a large pancake. Cook until a golden crust forms on the bottom and edges, 6 to 9 minutes. (Reduce heat, if necessary, to prevent burning.) To flip, carefully place a large plate or inverted rimless baking sheet over the pan and quickly invert. Slide the pancake back into the pan, browned-side now facing up. Continue to cook until the vegetables are tender and the bottom is golden, 3 to 4 minutes more.\n",
+                "Step 4: Slide the okonomiyaki onto a serving plate. Drizzle with the reserved mayonnaise mixture and sprinkle with sesame seeds and scallion greens.\n"
+            ]
+        },  
+        "Really Green Smoothie": {
+            "image_id": "image_id",
+            "ingredients": [
+                "1 large ripe banana",
+                "1 cup packed baby kale or coarsely chopped mature kale",
+                "1 cup unsweetened vanilla almond milk",
+                "¼ ripe avocado",
+                "1 tablespoon chia seeds",
+                "2 teaspoons honey",  
+                "1 cup ice cubes"
+            ],  
+            "directions": [
+                "Step 1: Combine banana, kale, almond milk, avocado, chia seeds and honey in a blender. Blend on high until creamy and smooth. Add ice and blend until smooth."
+            ]
+        },
+        "Greek Muffin-Tin Omelets with Feta & Peppers":{
+            "image_id": "image_id",
+            "ingredients": [
+                "Cooking spray",
+                "2 tablespoons extra-virgin olive oi",
+                "¾ cup diced onion",
+                "¼ teaspoon salt, divided",
+                "1 medium red bell pepper, diced",
+                "1 tablespoon finely chopped fresh oregano",  
+                "8 large eggs",
+                "¾ cup crumbled feta cheese",
+                "½ cup low-fat milk",
+                "½ teaspoon ground pepper",
+                "2 cups chopped fresh spinach",
+                "¼ cup sliced Kalamata olives",
+            ],  
+            "directions": [
+                "Step 1: Preheat oven to 325 degrees F. Liberally coat a 12-cup muffin tin with cooking spray.\n",
+                "Step 2: Heat oil in a large skillet over medium heat. Add onion and 1/8 teaspoon salt; cook, stirring, until starting to soften, about 3 minutes. Add bell pepper and oregano; cook, stirring, until the vegetables are tender and starting to brown, 4 to 5 minutes more. Remove from heat and let cool for 5 minutes.\n",
+                "Step 3: Whisk eggs, feta, milk, pepper and the remaining 1/8 teaspoon salt in a large bowl. Stir in spinach, olives and the vegetable mixture. Divide among the prepared muffin cups.\n",
+                "Step 4: Bake until firm to the touch, about 25 minutes. Let stand for 5 minutes before removing from the tin.\n"
+            ]
+        }
+    },
+    "grains": {
+        "Slow-Cooker Quinoa Salad with Arugula & Feta": {
+            "image_id": "image_id",
+            "ingredients": [
+                "2 ¼ cups unsalted vegetable stock",
+                "1 ½ cups uncooked quinoa, rinsed",
+                "1 cup sliced red onions (from 1 onion)",
+                "2 garlic cloves, minced (about 2 teaspoons)",
+                "1 (15.5 ounce) can no-salt-added chickpeas (garbanzo beans), drained and rinsed",
+                "2 ½ tablespoons olive oil",  
+                "¾ teaspoon kosher salt",
+                "2 teaspoons fresh lemon juice (from one lemon)",
+                "½ cup drained, chopped roasted red bell peppers (from jar)",
+                "4 cups baby arugula (about 4 ounces)",
+                "2 ounces feta cheese, crumbled (about 1/2 cup)",
+                "12 pitted kalamata olives, halved lengthwise",
+                "2 tablespoons coarsely chopped fresh oregano"
+            ],  
+            "directions": [
+                "Step 1: Stir together the stock, quinoa, onions, garlic, chickpeas, 1/2 tablespoon olive oil and 1/2 teaspoon salt in a 5- to 6-quart slow cooker. Cover and cook on LOW until the quinoa is tender and the stock is absorbed, 3 to 4 hours.\n", 
+                "Step 2: Turn off the slow cooker. Fluff the quinoa mixture with a fork. Whisk together the lemon juice and remaining 2 tablespoons olive oil and 1/4 teaspoon salt. Add the olive oil mixture and red bell peppers to the slow cooker; toss gently to combine. Gently fold in the arugula. Cover and let stand until the arugula is slightly wilted, about 10 minutes. Sprinkle each serving evenly with the feta cheese, olives and oregano.\n"
+            ]
+        },
+    "Smoked Turkey, Kale & Rice Bake": {
+        "image_id": "image_id",
+            "ingredients": [
+                "1 tablespoon extra-virgin olive oil",
+                "2 cups thinly sliced leeks, white and light green parts only",
+                "1 cup thinly sliced celery",
+                "4 cups slivered kale leaves",
+                "1 28-ounce can diced tomatoes",
+                "1 cup low-fat, no-salt-added cottage cheese",  
+                "1 cup instant or quick-cooking brown rice",
+                "6 ounces smoked turkey breast or smoked tofu, chopped (1 1/2 cups)",
+                "¼ cup water",
+                "1 teaspoon freshly ground pepper, or to taste",
+                "1 cup shredded extra-sharp Cheddar cheese"
+              ],
+            "directions": [
+                "Step 1: Heat oil in a large, broiler-safe skillet over medium-high heat. Add leeks and celery and cook, stirring frequently, until beginning to soften, 2 to 3 minutes. Add kale and tomatoes and cook, stirring, until the kale begins to wilt, 1 to 2 minutes. Stir in cottage cheese, rice, turkey (or tofu), water and pepper. Bring to a simmer. Reduce heat to medium-low, cover and cook for 10 minutes.\n",
+                "Step 2: Meanwhile, position rack in upper third of oven; preheat broiler.\n",
+                "Step 3: Stir the rice mixture, increase the heat to medium and cook, uncovered, until most of the liquid has evaporated, 10 to 12 minutes. Spread cheese on top. Broil until the cheese is bubbling, 2 to 3 minutes.\n"
+            ]
+        },
+        "Chile-Lime Veggie Noodles":{
+            "image_id": "image_id",
+            "ingredients": [
+                "8 ounces whole-wheat spaghetti or lo mein noodles",
+                "½ cup dry sherry",
+                "1 teaspoon freshly grated lime zest",
+                "2 tablespoons lime juice",
+                "3 tablespoons reduced-sodium soy sauce",
+                "2 tablespoons chile-garlic sauce, or to taste",  
+                "1 tablespoon light brown sugar",
+                "1 teaspoon canola oil plus 1 tablespoon, divided",
+                "4 large eggs, beaten",
+                "3 cups sliced shiitake mushroom caps",
+                "2 red, orange or yellow bell peppers, sliced",
+                "6 cups thinly sliced green cabbage",
+                "1 bunch scallions, sliced, divided"
+            ],  
+            "directions": [
+                "Step 1: Cook spaghetti (or noodles) according to package directions. Drain.\n",
+                "Step 2: Combine sherry, lime zest and juice, soy sauce, chile-garlic sauce and brown sugar in a small bowl. Set aside.\n",
+                "Step 3: Heat a 14-inch flat-bottom wok over high heat. Add 1 teaspoon oil and swirl to coat the pan. Add eggs and tilt wok to create an omelet. Cook until set, 30 seconds to 1 minute. Turn the omelet over and cook 30 seconds more. Remove to a clean cutting board and cut into strips.\n",
+                "Step 4: Add the remaining 1 tablespoon oil, mushrooms and bell peppers to the wok; stir-fry for 2 minutes. Add cabbage. Reserve about 2 tablespoons of the scallions for garnish and add the rest to the wok. Stir-fry until all the vegetables are tender-crisp, about 2 minutes more. Add the chile-lime sauce and the noodles; toss to coat. Add the omelet strips and stir-fry until heated through, 1 to 2 minutes more. Serve sprinkled with the reserved scallions.\n"
+            ]
+        }
+    },
+    "protien":{
+       "Sheet-Pan Roasted Salmon & Vegetables":{
+           "image_id": "image_id",
+           "ingredients": [
+               "1 pound fingerling potatoes, halved lengthwise",
+               "2 tablespoons olive oil",
+               "5 garlic cloves, coarsely chopped",
+               "½ teaspoon sea salt",
+               "½ teaspoon freshly ground black pepper",
+               "4 5 to 6-ounce fresh or frozen skinless salmon fillets",
+               "2 medium red, yellow and/or orange sweet peepers, cut into rings",
+               "2 cups cherry tomatoes",
+               "1 ½ cups chopped fresh parsley (1 bunch)",
+               "¼ cup pitted kalamata olives, halved",
+               "¼ cup finely snipped fresh oregano or 1 Tbsp. dried oregano, crushed",
+               "1 lemon"
+           ],
+           "directions":[
+               "Step 1: Preheat oven to 425 degrees F. Place potatoes in a large bowl. Drizzle with 1 Tbsp. of the oil and sprinkle with garlic and 1/8 tsp. of the salt and black pepper; toss to coat. Transfer to a large rimmed baking sheet; cover with foil. Roast 30 minutes.\n",
+               "Step 2: Meanwhile, thaw salmon, if frozen. Combine, in the same bowl, sweet peppers, tomatoes, parsley, olives, oregano and 1/8 tsp. of the salt and black pepper. Drizzle with remaining 1 Tbsp. oil; toss to coat.\n",
+               "Step 3: Rinse salmon; pat dry. Sprinkle with remaining 1/4 tsp. salt and black pepper. Spoon sweet pepper mixture over potatoes and top with salmon. Roast, uncovered, 10 minutes more or just until salmon flakes.\n",
+               "Step 4: Remove zest from lemon. Squeeze juice from lemon over salmon and vegetables. Sprinkle with zest.\n"
+           ]
+    },
+    "Grilled Lemon-Herb Chicken":{
+        "image_id":"image_id",
+        "ingredients": [
+            "6 boneless chicken breast halves (about 1 1/2 pounds total)",
+            "¼ cup olive oil",
+            "6 cloves garlic, minced",
+            "1 tablespoon lemon peel, finely shredded",
+            "2 teaspoons snipped fresh thyme",
+            "1 teaspoon snipped fresh rosemary",
+            "¼ teaspoon crushed red pepper",
+            "¼ teaspoon salt",
+            "⅛ teaspoon ground black pepper",
+            "1 Fresh thyme sprigs",
+            "1 Lemon wedges"
+        ],
+        "directions": [
+            "Step 1: Place chicken in a resealable plastic bag set in a shallow bowl. For marinade, in a small bowl, combine oil, garlic, lemon peel, thyme, rosemary, crushed red pepper, salt and black pepper. Pour marinade over chicken. Seal bag; turn to coat chicken. Marinate in the refrigerator for 2 to 4 hours, turning bag occasionally.\n",
+            "Step 2: Drain chicken, discarding marinade. Place chicken on the rack of an uncovered grill directly over medium coals. Grill for 12 to 15 minutes or until chicken is no longer pink (165 degrees F), turning once halfway through grilling. If desired, garnish with fresh thyme sprigs and lemon wedges.\n"
+        ]
+    },
+    "Oven BBQ Chicken": {
+        "image_id":"image_id",
+        "ingretients": [ 
+            "3 cups your favorite BBQ sauce",
+            "1/2 cups peach preserves",
+            "1 clove garlic, grated",
+            "Hot sauce",
+            "Olive oil for brushing",
+            "12 bone-in, skin-on chicken thighs",
+            "1 tsp. kosher salt",
+            "1/2 tsp. ground black pepper"
+        ],
+        "directions": [
+            "Step 1: In a medium saucepan, combine the BBQ sauce, peach preserves, garlic, and hot sauce. Heat over medium heat for 5 to 10 minutes, or until nice and hot. Set aside.\n",
+            "Step 2: Preheat oven to 400°F.\n",
+            "Step 3: Drizzle olive oil on 2 foil-lined rimmed baking sheets and place the chicken thighs skin-side down in the pans. Sprinkle the chicken evenly with the salt and pepper.\n",
+            "Step 4: Roast for 25 minutes. Remove from the oven briefly, brush sauce all over the thighs, then use a spatula to flip them over to the other side, being careful not to tear the skin. Brush the tops with more sauce and return to the oven for 7 minutes. Remove from oven, brush on more sauce, and return to the oven for another 7 minutes. Remove from oven, brush on sauce, and turn up the temperature to 425°F. Continue roasting for another 5 to 7 minutes, or until the sauce is starting to brown around the edges and the thighs are totally cooked through. Remove from the oven and let sit for at least 10 minutes before serving.\n"
+        ]
+    }
+    },
+"fruits": {
+    "Berry-Almond Smoothie Bowl": {
+        "image_id": "image_id",
+        "ingredients": [
+            "⅔ cup frozen raspberries",
+            "½ cup frozen sliced banana",
+            "½ cup plain unsweetened almond milk",
+            "5 tablespoons sliced almonds, divided",
+            "¼ teaspoon ground cinnamon",
+            "⅛ teaspoon ground cardamom",
+            "⅛ teaspoon vanilla extract",
+            "¼ cup blueberries",
+            "1 tablespoon unsweetened coconut flakes"
+        ],
+        "directions": [
+            "Step 1: Blend raspberries, banana, almond milk, 3 tablespoons almonds, cinnamon, cardamom and vanilla in a blender until very smooth.\n",
+            "Step 2: Pour the smoothie into a bowl and top with blueberries, the remaining 2 tablespoons almonds and coconut.\n"
+        ]
+    },
+    "Mixed Berry Smoothie": {
+        "image_id": "image_id",
+        "ingredients": [
+            "1 1/2 cups apple juice can also use almond milk, skim milk, coconut milk or other flavor of juice",
+            "1 banana sliced",
+            "1 1/2 cups frozen mixed berries",
+            "3/4 cup vanilla Greek yogurt",
+            "1 tablespoon honey optional",
+            "Optional garnish: fresh berries and mint sprigs"
+        ],
+        "directions": [
+            "Step 1: Place the apple juice, banana, berries and yogurt in a blender; blend until smooth. If the smoothie seems too thick, add a little more liquid (1/4 cup). \n",
+            "Step 2: Taste and add honey if desired. Pour into two glasses and garnish with fresh berries and mint sprigs if desired.\n"
+        ]
+    },
+
+    "Fruit Salad": {
+        "image_id":"image_id",
+        "ingredients":[
+            "⅔ cup fresh orange juice",
+            "⅓ cup fresh lemon juice",
+            "⅓ cup packed brown sugar",
+            "½ teaspoon grated orange zest",
+            "1 teaspoon vanilla extract",
+            "2 cups cubed fresh pineapple",
+            "2 cups strawberries, hulled and sliced",
+            "3 kiwi fruit, peeled and sliced",
+            "3 bananas, sliced ",
+            "2 oranges, peeled and sectioned",
+            "1 cup seedless grapes",
+            "2 cups blueberries"
+        ],
+        "directions": [
+            "Step 1: Gather the ingredients.\n",
+            "Step 2: For the sauce: Bring orange juice, lemon juice, brown sugar, orange zest, and lemon zest to a boil in a saucepan over medium-high heat. Reduce heat to medium-low and simmer until slightly thickened, about 5 minutes.\n",
+            "Step 3: Remove from heat and stir in vanilla extract. Set aside to cool.\n",
+            "Step 4: For the salad: Layer fruit in a large, clear glass bowl in this order: pineapple, strawberries, kiwi fruit, bananas, oranges, grapes, and blueberries. Pour cooled sauce over fruit; cover and refrigerate for 3 to 4 hours before serving.\n"
+        ]
+    }
+    
+},
+"diary": {
+    "Triple Berry Banana Yogurt Smoothie": {
+        "image_id":"image_id",
+        "ingredients": [
+            "8 ounces frozen mixed berries",
+            "2 small bananas, peeled, cut into 2-inch pieces, and frozen",
+            "6 ounces vanilla yogurt, preferably whole milk",
+            "1 cup whole milk",
+            "1 to 2 tablespoons honey"
+        ],
+        "directions": [
+            "Step 1: Add all ingredients to blender and puree until smooth. Pour into glasses and serve immediately.\n"
+        ]
+    },
+    "Three Cheese-Vegetable Quiche": {
+        "image_id":"image_id",
+        "ingredients": [
+            "1 crust from 1 box (14.1 oz) refrigerated Pillsbury™ Pie Crusts (2 Count), softened as directed on box",
+            "1 tablespoon vegetable oil",
+            "3 portabella mushrooms, sliced",
+            "1/2 red onion, sliced",
+            "1 cup milk",
+            "1 box (9 oz) frozen chopped spinach, thawed, squeezed to drain",
+            "1 teaspoon minced onion",
+            "1/3 cup egg whites (about 3 egg whites)",
+            "2 eggs",
+            "1/4 teaspoon salt",
+            "1/4 teaspoon pepper",
+            "1 cup Gouda cheese, shredded",
+            "3/4 cup Cheddar cheese, shredded",
+            "1 to 2 tablespoons Gorgonzola, crumbled",
+            "1/4 teaspoon dried parsley leaves"
+        ],
+        "directions": [
+            "Step 1: Heat oven to 350°F. Make 1 pie crust in 9-inch glass pie plate as directed on box for One-Crust-Filled Pie. Using fork, poke holes in the base all over and bake according to package directions.\n",
+            "Step 2: Meanwhile in medium skillet, heat oil. Add mushrooms and red onion slices; cook over medium-high heat until soft, about 4 to 5 minutes. Cool slightly.\n",
+            "Step 3: In medium bowl, combine milk, spinach, minced onion, egg whites, eggs, salt and pepper; stir until all ingredients are well combined.\n",
+            "Step 4: Stir in Gouda cheese, Cheddar cheese and cooled mushrooms mixture. Pour egg mixture into pie shell. Sprinkle Gorgonzola cheese over top. Bake 35 to 40 minutes or until center is puffed and light golden brown. Cool 10 minutes.\n"
+        ]
+    },
+    "Yogurt and Fruit Parfaits"
+}
+}
